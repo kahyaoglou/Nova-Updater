4258e84e98b71f52adf913ce0f1f50491abf1297
Furkan Eralp Kahyaoğlu <furkaneralpkahyaoglu@gmail.com>
2025-05-18 15:26:00 +0300

    An algorithm/structure was developed to detect the operating system and disable the Check for Windows Update button and the Update Microsoft Store Apps button on Windows devices running an operating system earlier than Windows 11.

--- Nova-Updater/Forms/MainPage.cs
+++ Nova-Updater/Forms/MainPage.cs
@@ -27,8 +27,19 @@ namespace Nova_Updater
         {
             ThemeManager.ApplyTheme(this);
             ThemeManager.UpdateLogo(pbxLogo, btnInfo);
+
             toolTipInfo.SetToolTip(btnInfo, "TR - Nova Updater, bilgisayarınızdaki uygulamaları güncel tutmanızı kolaylaştırır. Tek tıkla güncelleyin. Furkan Eralp Kahyaoğlu tarafından geliştirilmiştir.\n" +
                 "EN - Nova Updater, makes it easy to keep the applications on your computer up to date. Update them with a single click. Developed by Furkan Eralp Kahyaoğlu.");
+
+            string osVersion = OSVersionHelper.GetWindowsVersion();
+
+            if (osVersion != "Windows 11")
+            {
+                btnMicrosoftStoreApps.Enabled = false;
+                btnWindowsApps.Enabled = false;
+                btnMicrosoftStoreApps.Text = btnMicrosoftStoreApps.Text + $" (Coming soon for {osVersion})";
+                btnWindowsApps.Text = btnWindowsApps.Text + $" (Coming soon for {osVersion})";
+            }
         }
 
         private void btnUpdateAll_Click(object sender, EventArgs e)
---
+++ Nova-Updater/Helpers/OSVersionHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nova_Updater.Helpers
+{
+    public static class OSVersionHelper
+    {
+        public static string GetWindowsVersion()
+        {
+            Version version = Environment.OSVersion.Version;
+
+            if (version.Major == 10)
+            {
+                if (version.Build >= 22000)
+                    return "Windows 11";
+                else
+                    return "Windows 10";
+            }
+            else if (version.Major == 6)
+            {
+                if (version.Minor == 3)
+                    return "Windows 8.1";
+                else if (version.Minor == 2)
+                    return "Windows 8";
+                else if (version.Minor == 1)
+                    return "Windows 7";
+                else
+                    return $"Windows 6.{version.Minor} (unknown build {version.Build})";
+            }
+            else
+            {
+                return $"an older version of Windows (Version {version})";
+            }
+        }
+
+    }
+}
